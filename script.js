0fbad523e9d182425e074ff9e019f34d9eda253b
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2026-07-12 20:11:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(
+    `Hello, my name is ${this.name} and I am ${this.age} years old.`
+  );
 };
 
 function Employee(name, age, jobTitle) {
   Person.call(this, name, age);
-
   this.jobTitle = jobTitle;
 }
 
 Employee.prototype = Object.create(Person.prototype);
-
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function () {
@@ -23,13 +23,6 @@ Employee.prototype.jobGreet = function () {
   );
 };
 
-function runDemo() {
-  const person = new Person("Alice", 25);
-  person.greet();
-
-  const employee = new Employee("Bob", 30, "Manager");
-  employee.jobGreet();
-}
-
+// Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
